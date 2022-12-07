ecd845d963c9ac08d75a8ccf66649f3277cc2b2a
Keith Ma <keithchezzy@gmail.com>
2022-12-07 11:21:40 -0800

    about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,7 +2,10 @@ function About() {
 
     return(
         <div>
-            About
+            <h3>About Us</h3>
+            <h4>Welcome to MusicCloud</h4>
+            <p>Welcome to the wonderful world of MusicCloud streaming site. Where we curate the best of the best music made by music lovers and just for you! In MusicCloud, we offer direct links to Study Music and party music for your mood and taste for your liking. So come along and discover your next hit song for your everyday needs.</p>
+            <p1>Created and Curated By Ryan See and Keith Ma</p1>
         </div>
     )
 }
